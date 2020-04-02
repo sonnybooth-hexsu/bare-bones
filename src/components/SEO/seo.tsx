e3fd5881eec229f5e73ed71247264b8733bd6ff2
Sonny Booth <sb@amp.co>
2020-04-02 16:07:09 +0100

    typescript fixes

--- src/components/SEO/seo.tsx
+++ src/components/SEO/seo.tsx
@@ -1,9 +1,15 @@
 import React from "react"
-import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+type SEOProps = {
+  description: string
+  lang: string
+  meta: Array<any>
+  title: string
+}
+
+function SEO({ description, lang, meta, title }: SEOProps) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -69,13 +75,6 @@ SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
