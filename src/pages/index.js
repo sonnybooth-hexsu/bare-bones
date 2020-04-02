6e4c9da05e0d3fda826c9c76fb38708d251d3976
Sonny Booth <sb@amp.co>
2020-04-02 17:15:14 +0100

    tailwind setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,18 +9,18 @@ const IndexPage = () => (
       <h1 className="mb-12">Bare Bones</h1>
     </div>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-      <div className="bg-gray-300 h-12">Grid Item 1</div>
-      <div className="bg-gray-500 h-12">Grid Item 2</div>
-      <div className="bg-gray-300 h-12">Grid Item 3</div>
-      <div className="bg-gray-500 h-12">Grid Item 4</div>
-      <div className="bg-gray-300 h-12">Grid Item 5</div>
-      <div className="bg-gray-500 h-12">Grid Item 6</div>
-      <div className="bg-gray-300 h-12">Grid Item 7</div>
-      <div className="bg-gray-500 h-12">Grid Item 8</div>
-      <div className="bg-gray-300 h-12">Grid Item 9</div>
-      <div className="bg-gray-500 h-12">Grid Item 10</div>
-      <div className="bg-gray-300 h-12">Grid Item 11</div>
-      <div className="bg-gray-500 h-12">Grid Item 12</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 1</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 2</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 3</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 4</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 5</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 6</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 7</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 8</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 9</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 10</div>
+      <div className="bg-gray-300 h-12 p-4 text-center">Grid Item 11</div>
+      <div className="bg-gray-500 h-12 p-4 text-center">Grid Item 12</div>
     </div>
   </Layout>
 )
