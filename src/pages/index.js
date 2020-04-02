3b18d14a5016e673b1cbcb6d1eee702d4394a762
Sonny Booth <sb@amp.co>
2020-04-02 13:05:22 +0100

    removing assets for now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
-import SEO from "../components/seo"
+import SEO from "../components/SEO/seo"
 
 const IndexPage = () => (
   <Layout>
