aadf6c2c2872c6874b59a8a43e8bd28657c21e67
JessBrownless <jessicabrownless@gmail.com>
2020-04-05 15:40:58 +0100

    Bare Bones components

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,17 @@ import { ProductSlider } from "../components/ProductSlider"
 import lipsBefore from "../assets/images/lips-before.jpg"
 import lipsAfter from "../assets/images/lips-after.jpg"
 import lipsFillers from "../assets/images/lip-fillers.svg"
-import { Clock } from "react-feather"
+import treatmentHeader from "../assets/images/treatment-header.jpg"
+import signature from "../assets/images/signature.svg"
+import {
+  Clock,
+  Watch,
+  Calendar,
+  Tag,
+  Check,
+  CheckCircle,
+  CheckSquare,
+} from "react-feather"
 
 const IndexPage = () => (
   <Layout>
@@ -43,7 +53,7 @@ const IndexPage = () => (
           </span>{" "}
           - for blogs and articles
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 mb-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6 mb-12">
           <PostCard heading="Considerations for darker skin types when choosing in-clinic skin treatments" />
           <PostCard heading="Getting you wedding-ready!" />
           <PostCard heading="Considerations for darker skin types when choosing in-clinic skin treatments" />
@@ -55,7 +65,7 @@ const IndexPage = () => (
           </span>{" "}
           - for client reviews and such alike
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 mb-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mb-12">
           <TestimonialCard testimonial="I have had lip fillers with Yalda for a while now and the results are amazing. Not only is she professional, but she makes you feel at ease immediately. It’s been 6 months since my fillers and my lips are as juicy as ever!" />
           <TestimonialCard testimonial="Just had my lips done with Yalda and I absolutely love them. She’s absolutely brilliant and has done a fantastic job." />
           <TestimonialCard testimonial="I have had lip fillers with Yalda for a while now and the results are amazing. Not only is she professional, but she makes you feel at ease immediately. It’s been 6 months since my fillers and my lips are as juicy as ever!" />
@@ -112,9 +122,18 @@ const IndexPage = () => (
               provide her patients with satisfied, long lasting results:
             </p>
             <ul>
-              <li><Clock />Reducing the signs of ageing</li>
-              <li><Clock />Defining your facial features</li>
-              <li><Clock />Improving your overall skin health</li>
+              <li>
+                <Check className="inline mr-2" />
+                Reducing the signs of ageing
+              </li>
+              <li>
+                <CheckCircle className="inline mr-2" />
+                Defining your facial features
+              </li>
+              <li>
+                <CheckSquare className="inline mr-2" />
+                Improving your overall skin health
+              </li>
             </ul>
           </div>
         </div>
@@ -135,19 +154,21 @@ const IndexPage = () => (
           </span>
           - for before and after images of clients/products
         </h2>
-        <div className="grid pb-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4">
+        <div className="grid pb-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6">
           <ProductSlider leftImage={lipsBefore} rightImage={lipsAfter} />
           <ProductSlider leftImage={lipsBefore} rightImage={lipsAfter} />
           <ProductSlider leftImage={lipsBefore} rightImage={lipsAfter} />
           <ProductSlider leftImage={lipsBefore} rightImage={lipsAfter} />
         </div>
+        {/* Product Card Title */}
         <h2 className="text-2xl pt-8 pb-8">
           <span className="text-3xl md:text-4xl text-red-500 font-bold">
             Product Card
           </span>{" "}
           - Used to show off products and services
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-4 pb-12">
+        {/* Product Cards */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6 pb-12">
           <div className="bg-white">
             <div className="bg-red-200 pt-16 pb-16 flex justify-center">
               <img className="h-20" src={lipsFillers} />
@@ -192,7 +213,8 @@ const IndexPage = () => (
               </p>
               <a href="#">View Treatment</a>
             </div>
-          </div><div className="bg-white">
+          </div>
+          <div className="bg-white">
             <div className="bg-red-200 pt-16 pb-16 flex justify-center">
               <img className="h-20" src={lipsFillers} />
             </div>
@@ -207,9 +229,155 @@ const IndexPage = () => (
               <a href="#">View Treatment</a>
             </div>
           </div>
-          <h2>About This Treatment</h2>
-          <p>The revolutionary treatment has been around since the 1980’s and with very few risks and no recovery time it still remains one of the most popular non-surical cosmetic treatments. Botulinum Toxin injections minimises the appearance of lines and wrinkles by temporarily relaxing specific muscles, with results lasting 3 to 4 months. They are also used for treatment of hyperhidrosis (excess sweating) and bruxism (involuntary habitual grinding of the teeth).</p>
-          <div class></div>         
+        </div>
+        {/* Treatment Content Title */}
+        <h2 className="text-2xl pt-8 pb-8">
+          <span className="text-3xl md:text-4xl text-red-500 font-bold">
+            Treatment Content
+          </span>{" "}
+          - Used to describe a treatment or service
+        </h2>
+        {/* Treatment Content */}
+        <div className="grid grid-cols-1 grid md:grid-cols-12 gap-6 pb-12">
+          <div className="md:col-span-8">
+            <h2>About This Treatment</h2>
+            <p>
+              The revolutionary treatment has been around since the 1980’s and
+              with very few risks and no recovery time it still remains one of
+              the most popular non-surical cosmetic treatments. Botulinum Toxin
+              injections minimises the appearance of lines and wrinkles by
+              temporarily relaxing specific muscles, with results lasting 3 to 4
+              months. They are also used for treatment of hyperhidrosis (excess
+              sweating) and bruxism (involuntary habitual grinding of the
+              teeth).
+            </p>
+            <div className="border-solid border-l-4 border-red-500 pl-6 md:pr-48 md:ml-10">
+              <h5 className="text-2xl leading-tight mt-8 mb-8 md:text-3xl">
+                &quot;I have had lip fillers with Yalda for a while now and the
+                results are amazing. Not only is she professional but she makes
+                you feel at ease. It’s been nearly 6 months since my fillers and
+                my lips are still juicy as ever!&quot;
+              </h5>
+            </div>
+            <p className="pb-8">
+              The revolutionary treatment has been around since the 1980’s and
+              with very few risks and no recovery time it still remains one of
+              the most popular non-surical cosmetic treatments. Botulinum Toxin
+              injections minimises the appearance of lines and wrinkles by
+              temporarily relaxing specific muscles, with results lasting 3 to 4
+              months. They are also used for treatment of hyperhidrosis (excess
+              sweating) and bruxism (involuntary habitual grinding of the
+              teeth).
+            </p>
+          </div>
+          {/* Overview Attributes */}
+          <div className="md:col-start-10 md:col-span-3">
+            <dl className="mb-8">
+              <dt className="pb-4 pt-4 border-solid border-t-2 border-gray-900 flex justify-between">
+                <div>
+                  <Clock className="inline mr-2 text-gray-900" />
+                  <p className="inline font-bold">Treatment Time</p>
+                </div>
+                <dd className="text-right inline">10 Minutes</dd>
+              </dt>
+              <dt className="pb-4 pt-4 border-solid border-t-2 border-gray-900 flex justify-between">
+                <div>
+                  <Watch className="inline mr-2 text-gray-900" />
+                  <p className="inline font-bold">Downtime</p>
+                </div>
+                <dd className="text-right inline">One day</dd>
+              </dt>
+              <dt className="pb-4 pt-4 border-solid border-t-2 border-gray-900 flex justify-between">
+                <div>
+                  <Calendar className="inline mr-2 text-gray-900" />
+                  <p className="inline font-bold">Results Last</p>
+                </div>
+                <dd className="text-right inline">3–4 Months</dd>
+              </dt>
+              <dt className="pb-4 pt-4 border-solid border-t-2 border-gray-900 border-solid border-b-2 border-gray-900 flex justify-between">
+                <div>
+                  <Tag className="inline mr-2 text-gray-900" />
+                  <p className="inline font-bold">Price</p>
+                </div>
+                <dd className="text-right inline">Staring from £100</dd>
+              </dt>
+            </dl>
+          </div>
+        </div>
+        {/* Treatment Content Title */}
+        <h2 className="text-2xl pt-8 pb-8">
+          <span className="text-3xl md:text-4xl text-red-500 font-bold">
+            About Content Block
+          </span>{" "}
+          - I dont know
+        </h2>
+        {/* About Content */}
+        <div className="grid grid-cols-1 grid md:grid-cols-2 gap-6">
+          <div>
+            <h2 className="text-2xl leading-tight tracking-wide pb-0 md:text-3xl">
+              Dr. Yalda Jamali is a qualified medical doctor specialising in
+              aesthetic medicine. She runs a successful clinic and mentors at
+              the Harley Academy.
+            </h2>
+          </div>
+          <div>
+            <p className="pb-4">
+              She graduated from the University of Manchester with a Bachelor of
+              Medicine, Bachelor of Surgery (MBChB) and holds a Level 7
+              postgraduate qualification in facial injectables. Dr Yalda is
+              passionate about ensuring aesthetics is a carefully regulated
+              industry, conducted with extreme precision, diligence and care.{" "}
+            </p>
+            <p className="pb-4">
+              She has a key artistic eye for detail. Her clients regard her as a
+              personable, results-oriented aesthetics doctor. Dr Yalda goes
+              above and beyond to ensure that her clients are completely
+              satisfied by their results. In addition to running a successful
+              clinic in Nottingham, Dr Yalda is a mentor at the Harley Academy
+              in London for medical professionals wishing to enter the
+              aesthetics field. In her capacity as a mentor, Dr Yalda provides
+              her insights and expert observations into the aesthetics industry
+              to the next generation of medical aesthetic professionals.
+            </p>
+            <p className="pb-4">
+              In addition to running a successful clinic in Nottingham, Dr Yalda
+              is a mentor at the Harley Academy in London for medical
+              professionals wishing to enter the aesthetics field. In her
+              capacity as a mentor, Dr Yalda provides her insights and expert
+              observations into the aesthetics industry to the next generation
+              of medical aesthetic professionals.
+            </p>
+            <p className="pb-4">
+              Dr Yalda also demonstrates her clinical skills at the GILD Clinic
+              in London, focusing on the practical application of facial
+              injectables and skin rejuvenation.
+            </p>
+            <img className="pt-5 h-20" src={signature} />
+          </div>
+        </div>
+        {/* 50/50 Header Title */}
+        <h2 className="text-2xl pt-8 pb-8">
+          <span className="text-3xl md:text-4xl text-red-500 font-bold">
+            50/50 Header
+          </span>{" "}
+          - Display a 50% hero image and text
+        </h2>
+        {/* Final Container Div */}
+      </div>
+      {/* 50/50 Header */}
+      <div className="grid grid-rows-2 md:grid-cols-2">
+        <div className="flex flex-col justify-center bg-gray-300 pt-20 pb-20 pr-8 pl-8 md:pt-0 md:pb-0 md:h-full md:pr-12 md:pl-12 lg:pr-20 lg:pl-20 xl:pr-50 xl:pl-50">
+          <h1>Soft Tissue Fillers</h1>
+          <p className="pb-6">
+            Instantly soften fine lines & wrinkles, restore age-related volume
+            loss and augment and contour facial features.
+          </p>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Book a consultation
+          </button>
+        </div>
+        <div className="md:h-full">
+          <img src={treatmentHeader} />
         </div>
       </div>
     </div>
