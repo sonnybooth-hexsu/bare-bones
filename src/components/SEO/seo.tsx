828367aa608f3e3a6ab56a5e45090789006dc753
Sonny Booth <sb@amp.co>
2020-04-06 19:13:23 +0100

    link repeat fix

--- src/components/SEO/seo.tsx
+++ src/components/SEO/seo.tsx
@@ -74,9 +74,7 @@ export const SEO = ({ description, lang, meta, title }: SEOProps) => {
                 content: metaDescription,
               },
             ].concat(meta)}
-          >
-            <link rel="stylesheet" href="https://use.typekit.net/nbx8okc.css" />
-          </Helmet>
+          ></Helmet>
         )
       }}
     </ThemeContext.Consumer>
