98cc639a7b5486881ee07a6201e8aec1dd6ae4c9
Sonny Booth <sb@amp.co>
2020-04-13 14:56:37 +0100

    base slider

--- src/components/Carousel/carousel.tsx
+++ src/components/Carousel/carousel.tsx
@@ -5,11 +5,26 @@ import "./slick-overrides.module.css"
 export const Carousel = ({ children }) => {
   const settings = {
     dots: true,
-    infinite: true,
     arrows: false,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
     slidesToScroll: 1,
+    slidesToShow: 3,
+    swipeToSlide: true,
+    responsive: [
+      {
+        breakpoint: 1024, //Tailwind lg default
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768, //Tailwind md default
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   }
 
   return <Slider {...settings}>{children}</Slider>
